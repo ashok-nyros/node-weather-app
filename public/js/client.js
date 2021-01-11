2ae3f2f9a2d82e6cf7fc7eb87838da0320298ef9
nyros <ashok.nyros@gmail.com>
2021-01-11 14:02:10 +0530

    styles addes

--- public/js/client.js
+++ public/js/client.js
@@ -1,10 +1,31 @@
 let address = document.getElementById("location");
+address.addEventListener("input", checkWeatherString)
 let getBtn = document.getElementById("get-btn");
 let reportId = document.getElementById("weatherReport");
-let error = document.getElementById("error")
+let error = document.getElementById("error");
+let errormessage = document.getElementById("error-message")
 
 getBtn.addEventListener('click', getWeatherData);
 var letters = /^[A-Za-z ]+$/;
+
+function checkWeatherString(e) {
+    console.log(e.target.value);
+    if (e.target.value.length < 1) {
+        errormessage.innerHTML =
+            '<p>Please enter location to check weather!</p>'
+    } else {
+        errormessage.innerHTML = "";
+    }
+    if (e.target.value.length > 1) {
+        if (!e.target.value.match(letters)) {
+            errormessage.innerHTML =
+                '<p>Please enter valid text!</p>'
+        } else {
+            errormessage.innerHTML = "";
+        }
+    }
+
+}
 //to get weather report based on location
 function getWeatherData(e) {
     reportId.innerHTML = "";
@@ -12,43 +33,48 @@ function getWeatherData(e) {
     if (!address.value) {
         e.preventDefault();
         address.focus();
-        return alert("please enter a location");
-    }
-    if (address.value.match(letters)) {
-        getBtn.innerHTML = 'Get Weather Report ' +
-            '<i class="fa fa-spinner fa-spin" style="font-size:18px"></i>'
-        fetch(`/weather?address=${address.value}`).then((response) => {
-            response.json().then((data) => {
-                getBtn.innerHTML = "Get Weather Report";
-                if (data.error) {
-                    console.log("error is " + data.error);
-                    address.value = "";
-                    error.classList.add('card')
-                    error.innerHTML = '<div class="card-body">' +
-                        "OOPS! " + data.error +
-                        '</div>'
-
-                } else {
-                    address.value = "";
-                    let weatherClassName = "";
-                    console.log(data);
-                    weatherClassName = getWeatherClassName(data.forecast.weatherCode);
-                    reportId.innerHTML =
-                        '<h6>' + data.location + '</h6>' +
-                        '<p id="temp">' + data.forecast.temp + '&#8451;</p>' +
-                        '<div class="icon">' +
-                        '<i class="wi ' + weatherClassName + '" aria-hidden="true"></i>' +
-                        '</div>' +
-                        '<p>' + data.forecast.desc + '</p>' +
-                        '<p>' + data.forecast.localtime + '</p>' +
-                        '<p>Pressure :' + data.forecast.pressure + ' &nbsp;&nbsp; wind :' + data.forecast.pressure + '</p>'
-                }
-            });
-        });
-        weatherClassName = "";
+        errormessage.innerHTML =
+            '<p>Please enter location to check weather</p>'
+            // return alert("please enter a location");
     } else {
-        alert("Location should not contain numbers and special characters!");
-        address.value = "";
+        if (address.value.match(letters)) {
+            getBtn.innerHTML = 'Get Weather Report ' +
+                '<i class="fa fa-spinner fa-spin" style="font-size:18px"></i>'
+            fetch(`/weather?address=${address.value}`).then((response) => {
+                response.json().then((data) => {
+                    getBtn.innerHTML = "Get Weather Report";
+                    if (data.error) {
+                        console.log("error is " + data.error);
+                        address.value = "";
+                        error.classList.add('card')
+                        error.innerHTML = '<div class="card-body">' +
+                            "OOPS! " + data.error +
+                            '</div>'
+
+                    } else {
+                        address.value = "";
+                        let weatherClassName = "";
+                        console.log(data);
+                        weatherClassName = getWeatherClassName(data.forecast.weatherCode);
+                        reportId.innerHTML =
+                            '<h6>' + data.location + '</h6>' +
+                            '<p id="temp">' + data.forecast.temp + '&#8451;</p>' +
+                            '<div class="icon">' +
+                            '<i class="wi ' + weatherClassName + '" aria-hidden="true"></i>' +
+                            '</div>' +
+                            '<p>' + data.forecast.desc + '</p>' +
+                            '<p>' + data.forecast.localtime + '</p>' +
+                            '<p>Pressure :' + data.forecast.pressure + ' &nbsp;&nbsp; wind :' + data.forecast.pressure + '</p>'
+                    }
+                });
+            });
+            weatherClassName = "";
+        } else {
+            errormessage.innerHTML =
+                '<p>Please enter valid text!</p>'
+                // alert("Location should not contain numbers and special characters!");
+            address.value = "";
+        }
     }
 }
 //to adding weather image dynamically
